a6d475950f66bba75668e168bd1ef4b386f0bc75
Cooley <npc19@Wrightlab-mnb-1.fios-router.home>
2018-07-18 22:05:40 -0400

    Additional Functions

---
+++ R/GFFParser.R
@@ -0,0 +1,79 @@
+#' A function for parsing genomic General Format Files
+#' @param GFFAddress a matrix of lists, the upper triange of which is filled with matrices of putative homologs
+#' @keywords GFF
+#' @export
+#' @examples
+#' GFFParser()
+
+
+GFFParser <- function(GFFAddress,
+                      Verbose = FALSE) {
+  GeneCalls <- vector("list",
+                      length = length(x))
+  if (Verbose == TRUE) {
+    TimeStart <- Sys.time()
+    pBar <- txtProgressBar(style = 1L)
+  }
+  for (i in seq_along(GFFAddress)) {
+    z1 <- gzcon(url(GFFAddress[i]))
+    z2 <- textConnection(readLines(z1))
+    z3 <- readLines(z2)
+    z4 <- strsplit(z3,
+                   split = "\t")
+    Start <- sapply(z4,
+                    function(x) ifelse(test = length(x) == 9L & x[3] == "CDS",
+                                       yes = x[4],
+                                       no = NA),
+                    USE.NAMES = FALSE)
+    Stop <- sapply(z4,
+                   function(x) ifelse(test = length(x) == 9L & x[3] == "CDS",
+                                      yes = x[5],
+                                      no = NA),
+                   USE.NAMES = FALSE)
+    Strand <- sapply(z4,
+                     function(x) ifelse(test = length(x) == 9L & x[3] == "CDS",
+                                        yes = x[7],
+                                        no = NA),
+                     USE.NAMES = FALSE)
+    Product <- sapply(z4,
+                      function(x) ifelse(test = length(x) == 9L & x[3] == "CDS",
+                                         yes = x[9],
+                                         no = NA),
+                      USE.NAMES = FALSE)
+    Product <- str_extract(Product,
+                           "(?<=product=)(.*)(?=;protein_id)")
+    z5 <- as.integer(Start[which(!is.na(Start) &
+                                   !is.na(Stop) &
+                                   !is.na(Strand) &
+                                   !is.na(Product))])
+    z6 <- as.integer(Stop[which(!is.na(Start) &
+                                  !is.na(Stop) &
+                                  !is.na(Strand) &
+                                  !is.na(Product))])
+    z7 <- Strand[which(!is.na(Start) &
+                         !is.na(Stop) &
+                         !is.na(Strand) &
+                         !is.na(Product))]
+    z8 <- Product[which(!is.na(Start) &
+                          !is.na(Stop) &
+                          !is.na(Strand) &
+                          !is.na(Product))]
+    GeneCalls[[i]] <- data.frame("Start" = z5,
+                                 "Stop" = z6,
+                                 "Strand" = z7,
+                                 "Annotation" = z8,
+                                 stringsAsFactors = FALSE)
+    if (Verbose == TRUE) {
+      setTxtProgressBar(pb = pBar,
+                        value = i/length(GFFAddress))
+    }
+  }
+  if (Verbose == TRUE) {
+    TimeStop <- Sys.time()
+    TotalTime <- (TimeStop - TimeStart)
+    cat("\n")
+    print(TotalTime)
+  }
+  return(GeneCalls)
+}
+
---
+++ R/LogicalPan.R
@@ -0,0 +1,86 @@
+#' A function for parsing genomic General Format Files
+#' @param HomologList a list of matrices that represents sets of homologs
+#' @param GeneCalls a list of matrices that represent gene calls
+#' @keywords Homologs
+#' @export
+#' @examples
+#' LogicalPan()
+
+LogicalPan <- function(HomologList, 
+                       GeneCalls,
+                       Verbose = FALSE,
+                       Plot = FALSE) {
+  InitialMatrix <- vector("list",
+                          length = length(HomologList))
+  CatchMatrix <- vector("list",
+                        length = length(HomologList))
+  if (Verbose == TRUE) {
+    pBar <- txtProgressBar(style = 1L)
+    TimeStart <- Sys.time()
+  }
+  for (i in seq_along(InitialMatrix)) {
+    InitialMatrix[[i]] <- vector("logical",
+                                 length = ncol(HomologList[[i]]))
+    CatchMatrix[[i]] <- vector("integer",
+                               length = ncol(HomologList[[i]]))
+    for (j in seq_along(InitialMatrix[[i]])) {
+      if (any(!is.na(HomologList[[i]][, j]))) {
+        InitialMatrix[[i]][j] <- TRUE
+        CatchMatrix[[i]][j] <- unique(HomologList[[i]][which(!is.na(HomologList[[i]][, j])), j])
+      }
+    }
+    if (Verbose == TRUE) {
+      setTxtProgressBar(pb = pBar,
+                        value = i/length(InitialMatrix))
+    }
+  }
+  InitialMatrix <- do.call(cbind,
+                           InitialMatrix)
+  CatchMatrix <- do.call(cbind,
+                         CatchMatrix)
+  GeneRows <- sapply(GeneCalls,
+                     function(x) nrow(x))
+  MatRows <- vector("integer",
+                    length = length(GeneCalls))
+  for (i in seq_along(GeneCalls)) {
+    MatRows[i] <- length(unique(CatchMatrix[i, which(!is.na(CatchMatrix[i, ]))]))
+    if (is.na(MatRows[i])) {
+      MatRows[i] <- 0L
+    }
+  }
+  AddRows <- GeneRows - MatRows
+  AdditionalMatrix <- matrix(FALSE,
+                             ncol = sum(AddRows),
+                             nrow = length(GeneCalls))
+  Count <- 1L
+  for (i in seq_along(AddRows)) {
+    CurrentRows <- AddRows[i]
+    AdditionalMatrix[i, (Count:(Count + CurrentRows - 1L))] <- TRUE
+    Count <- Count + CurrentRows
+  }
+  PanMatrix <- cbind(InitialMatrix,
+                     AdditionalMatrix)
+  PanMatrix <- PanMatrix[, order(colSums(PanMatrix),
+                                 decreasing = TRUE)]
+  PanMatrix <- t(PanMatrix)
+  if (Verbose == TRUE) {
+    cat("\n")
+    TimeStop <- Sys.time()
+    TotalTime <- (TimeStop - TimeStart)
+    print(TotalTime)
+  }
+  if (Plot == TRUE) {
+    image(PanMatrix,
+          col = c("white", "blue"))
+  }
+  return(PanMatrix)
+}
+
+
+
+
+
+
+
+
+
